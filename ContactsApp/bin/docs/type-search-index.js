ae9ae02fcbbf5f12459ef2dd367f03944d82c6c4
Toni Saarinen <toni.saarinen1234@gmail.com>
2024-01-04 23:53:29 +0200

    Initial commit

---
+++ ContactsApp/bin/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ContactsApp"},{"p":"<Unnamed>","l":"ContactsFilesSaver"},{"p":"<Unnamed>","l":"ContactsManager"},{"p":"<Unnamed>","l":"ContactsReader"}];updateSearchResults();
