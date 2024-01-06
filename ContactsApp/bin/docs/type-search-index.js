762a0a6a4791bdb843278754efb6077258499fb0
saarinentoni <144778885+saarinentoni@users.noreply.github.com>
2024-01-06 13:39:57 +0200

    Delete ContactsApp directory

--- ContactsApp/bin/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ContactsApp"},{"p":"<Unnamed>","l":"ContactsFilesSaver"},{"p":"<Unnamed>","l":"ContactsManager"},{"p":"<Unnamed>","l":"ContactsReader"}];updateSearchResults();
